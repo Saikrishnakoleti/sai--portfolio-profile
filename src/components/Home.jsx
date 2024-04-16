18db6b730fb5b054ffad23e06c7b5eee1a1e63d5
Saikrishnakoleti <saikrishna.koleti@gmail.com>
2024-04-16 16:29:28 +0530

    my portfolio changes

---
+++ src/components/Home.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import Typewriter from 'typewriter-effect';
+import Fade from 'react-reveal';
+import endpoints from '../constants/endpoints';
+import Social from './Social';
+import FallbackSpinner from './FallbackSpinner';
+
+const styles = {
+  nameStyle: {
+    fontSize: '5em',
+  },
+  inlineChild: {
+    display: 'inline-block',
+  },
+  setColor: {
+    color: 'blue'
+  },
+  mainContainer: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+};
+
+function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(endpoints.home, {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => setData(res))
+      .catch((err) => err);
+  }, []);
+
+  return data ? (
+    <Fade>
+      <div style={styles.mainContainer}>
+        <h1 style={styles.nameStyle}>{data?.name}</h1>
+        <div style={{ flexDirection: 'row' }}>
+          <h2 style={styles.inlineChild}>I&apos;m&nbsp;</h2>
+          <Typewriter
+            options={{
+              loop: true,
+              autoStart: true,
+              strings: data?.roles,
+            }}
+          />
+        </div>
+        <h5><strong>Email: </strong><a href='https://mail.google.com/' target="_blank">koleti.krishna11@gmail.com</a></h5>
+        <h5><strong>Contact: </strong> <span style={styles.setColor}>8985438010</span></h5>
+        <Social />
+      </div>
+    </Fade>
+  ) : <FallbackSpinner />;
+}
+
+export default Home;
